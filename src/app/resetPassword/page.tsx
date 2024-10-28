94cf77890bdeb8c3818549d628339cabddb840e2
Ali-hasan-sss <emonate8@gmail.com>
2024-10-28 22:27:44 +0300

    add reset password

--- src/app/resetPassword/page.tsx
+++ src/app/resetPassword/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useContext, useState } from "react";
+import React, { useEffect, useContext, useState, Suspense } from "react";
 import axios from "axios";
 import { useRouter, useSearchParams } from "next/navigation";
 import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
@@ -14,16 +14,25 @@ export default function ResetPassword() {
   const [showConfirmPassword, setShowConfirmPassword] =
     useState<boolean>(false);
   const [errorMessage, setErrorMessage] = useState<string>("");
+  const [token, setToken] = useState<string | null>(null);
+  const [userId, setUserId] = useState<string | null>(null);
   const { isDarkMode } = useContext(ThemeContext);
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  // استخراج التوكن وuserId وكلمة المرور القديمة المشفرة من الـ URL
-  const token = searchParams.get("token");
-  const userId = searchParams.get("id");
-  const oldPassword = searchParams.get("oldPassword");
+  useEffect(() => {
+    const fetchedToken = searchParams.get("token");
+    const fetchedUserId = searchParams.get("id");
 
-  const validatePassword = (password: string): boolean => password.length >= 8;
+    if (!fetchedToken || !fetchedUserId) {
+      setErrorMessage("المعلومات المطلوبة غير متوفرة.");
+      return; // الخروج من الدالة إذا كانت المعلومات غير موجودة
+    }
+    setToken(fetchedToken);
+    setUserId(fetchedUserId);
+  }, [searchParams]);
+
+  const validatePassword = (password: string) => password.length >= 8;
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -39,95 +48,100 @@ export default function ResetPassword() {
     }
 
     try {
-      // إرسال الطلب إلى الخادم مع التوكن في الهيدر
-      await axios.post(
-        `${API_BASE_URL}/users/reset-password`,
-        { newPassword: password, id: userId, token: token } // البيانات المرسلة
-      );
+      await axios.post(`${API_BASE_URL}/users/reset-password`, {
+        newPassword: password,
+        id: userId,
+        token: token,
+      });
 
-      router.push("/"); // تحويل إلى الصفحة الرئيسية عند النجاح
+      router.push("/");
     } catch (error) {
       setErrorMessage("حدث خطأ أثناء إعادة تعيين كلمة المرور");
     }
   };
 
+  // عرض رسالة الخطأ إذا كانت موجودة
+  if (errorMessage) {
+    return <div>{errorMessage}</div>;
+  }
+
   return (
-    <div
-      className={`flex justify-center items-center h-screen ${
-        isDarkMode ? "bg-gray-200 text-black" : "bg-gray-800 text-light"
-      }`}
-    >
-      <form
-        onSubmit={handleSubmit}
-        className="p-8 rounded shadow-md w-full max-w-sm bg-white"
+    <Suspense fallback={<div>Loading...</div>}>
+      <div
+        className={`flex justify-center items-center h-screen ${
+          isDarkMode ? "bg-gray-200 text-black" : "bg-gray-800 text-light"
+        }`}
       >
-        <h2 className="text-2xl font-bold mb-4 text-center">
-          تعيين كلمة مرور جديدة
-        </h2>
-
-        {errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>}
-
-        <div className="mb-4">
-          <label htmlFor="password" className="block font-bold mb-2">
-            كلمة المرور الجديدة
-          </label>
-          <div className="relative">
-            <input
-              type={showPassword ? "text" : "password"}
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
-              required
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="absolute left-2 top-1 mt-2 mr-2"
-            >
-              {showPassword ? (
-                <EyeSlashIcon className="h-5 w-5 text-gray-500" />
-              ) : (
-                <EyeIcon className="h-5 w-5 text-gray-500" />
-              )}
-            </button>
-          </div>
-        </div>
-
-        <div className="mb-4">
-          <label htmlFor="confirmPassword" className="block font-bold mb-2">
-            تأكيد كلمة المرور
-          </label>
-          <div className="relative">
-            <input
-              type={showConfirmPassword ? "text" : "password"}
-              id="confirmPassword"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
-              required
-            />
-            <button
-              type="button"
-              onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-              className="absolute left-2 top-1 mt-2 mr-2"
-            >
-              {showConfirmPassword ? (
-                <EyeSlashIcon className="h-5 w-5 text-gray-500" />
-              ) : (
-                <EyeIcon className="h-5 w-5 text-gray-500" />
-              )}
-            </button>
-          </div>
-        </div>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+        <form
+          onSubmit={handleSubmit}
+          className="p-8 rounded shadow-md w-full max-w-sm bg-white"
         >
-          تحديث كلمة المرور
-        </button>
-      </form>
-    </div>
+          <h2 className="text-2xl font-bold mb-4 text-center">
+            تعيين كلمة مرور جديدة
+          </h2>
+
+          <div className="mb-4">
+            <label htmlFor="password" className="block font-bold mb-2">
+              كلمة المرور الجديدة
+            </label>
+            <div className="relative">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute left-2 top-1 mt-2 mr-2"
+              >
+                {showPassword ? (
+                  <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+                ) : (
+                  <EyeIcon className="h-5 w-5 text-gray-500" />
+                )}
+              </button>
+            </div>
+          </div>
+
+          <div className="mb-4">
+            <label htmlFor="confirmPassword" className="block font-bold mb-2">
+              تأكيد كلمة المرور
+            </label>
+            <div className="relative">
+              <input
+                type={showConfirmPassword ? "text" : "password"}
+                id="confirmPassword"
+                value={confirmPassword}
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="w-full px-3 py-2 border rounded focus:outline-none focus:border-blue-500"
+                required
+              />
+              <button
+                type="button"
+                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                className="absolute left-2 top-1 mt-2 mr-2"
+              >
+                {showConfirmPassword ? (
+                  <EyeSlashIcon className="h-5 w-5 text-gray-500" />
+                ) : (
+                  <EyeIcon className="h-5 w-5 text-gray-500" />
+                )}
+              </button>
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
+          >
+            تحديث كلمة المرور
+          </button>
+        </form>
+      </div>
+    </Suspense>
   );
 }
